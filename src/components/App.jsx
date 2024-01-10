97dc2ae04e9b7501ef13d232b6ffb540cdff129b
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-10 15:26:25 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+
 import "./App.css";
 import { CustomButton } from "./CustomButton/CustomButton";
 function App() {
@@ -11,16 +12,22 @@ function App() {
   };
 
   const [clicks, setClicks] = useState(0);
-  const handleClick3 = () => {
+  const handleClick4 = () => {
     setClicks(clicks + 1);
   };
 
+  const [isOpen, setIsOpen] = useState(false);
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
       <button onClick={handleClick1}>handleClick1</button>
       <button onClick={handleClick2}>handleClick2</button>
       <CustomButton message="Playing music!">Play some music</CustomButton>
-      <button onClick={handleClick3}>Current: {clicks}</button>
+      <button onClick={handleClick4}>Current: {clicks}</button>
+      <button onClick={handleToggle}>{isOpen ? "Hide" : "Show"}</button>
+      {isOpen && <div>Now you can see me!</div>}
     </>
   );
 }
