14410f5e308c219c81ec12941dc9733c42f861e3
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-11 00:22:35 +0100

    conf
    // https://vitejs.dev/config/

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 // import { Modal } from "./Modal/Modal";
 
-import "./App.css";
+import './App.css';
 
 const App = () => {
   const [clicks, setClicks] = useState(() => {
-    const savedClicks = window.localStorage.getItem("saved-clicks");
+    const savedClicks = window.localStorage.getItem('saved-clicks');
     if (savedClicks !== null) {
       return parseInt(savedClicks, 10); // Преобразование строки в число
     }
@@ -13,14 +13,12 @@ const App = () => {
   });
 
   useEffect(() => {
-    window.localStorage.setItem("saved-clicks", clicks);
+    window.localStorage.setItem('saved-clicks', clicks);
   }, [clicks]);
 
   return (
     <div>
-      <button onClick={() => setClicks(clicks + 1)}>
-        You clicked {clicks} times
-      </button>
+      <button onClick={() => setClicks(clicks + 1)}>You clicked {clicks} times</button>
       <button onClick={() => setClicks(0)}>Reset</button>
     </div>
   );
